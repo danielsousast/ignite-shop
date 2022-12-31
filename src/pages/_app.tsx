e6792c3b69a8adc8289b471297afee017d170a69
Daniel <daniel@Daniels-iMac.local>
2022-12-31 17:58:50 -0300

    feat: project base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { gobalStyles } from '../styles/global'
+
+gobalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,10 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+// import { Container } from './styles';
+
+const ProductPage: React.FC = () => {
+	const { query } = useRouter()
+	return <div />;
+}
+
+export default ProductPage;
